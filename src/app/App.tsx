7578123e058341f903e741b395c9be39447f2b31
zeCode <yassineamine788@gmail.com>
2024-09-01 16:04:50 +0200

    added vercel analytics

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -4,6 +4,7 @@ import { Hero } from "./components/Hero"; // Import your Home component
 import { ThemeProvider } from "@/components/ui/theme-provider";
 import { Intro } from "./components/Intro";
 
+
 const App = () => {
   return (
     <ThemeProvider defaultTheme="dark" storageKey="ui-theme">
